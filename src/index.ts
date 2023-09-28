6f95ac24fb0bc416424c43394edd8381180b6c87
aeff60 <ssbunklang@gmail.com>
2023-09-28 12:54:51 +0700

    feat: Set up Elysia server with basic routes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get("/", () => "Hello Elysia")
+  .get("/ping", () => "pong")
+  .get("/hello", () => "Hello World")
+  .get("/calculate", (req) => {
+    const num1 = parseInt(req.query.num1, 10) || 0;
+    const num2 = parseInt(req.query.num2, 10) || 0;
+    const sum = num1 + num2;
+    return `The sum of ${num1} and ${num2} is: ${sum}`;
+  })
+  .listen(5000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
